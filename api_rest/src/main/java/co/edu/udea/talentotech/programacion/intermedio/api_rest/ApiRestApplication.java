015ae394ec87dc94d79cd2e52f91e386ba717e4a
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-04 16:49:40 -0500

    Add environment variable logging to ApiRestApplication

--- api_rest/src/main/java/co/edu/udea/talentotech/programacion/intermedio/api_rest/ApiRestApplication.java
+++ api_rest/src/main/java/co/edu/udea/talentotech/programacion/intermedio/api_rest/ApiRestApplication.java
@@ -8,6 +8,11 @@ public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
+		System.out.println(System.getenv("DB_HOST"));
+		System.out.println(System.getenv("DB_PORT"));
+		System.out.println(System.getenv("DB_NAME"));
+		System.out.println(System.getenv("DB_USER"));
+		System.out.println(System.getenv("DB_PASSWORD"));
 	}
 
 }
