0d450f940daa63b6bddeac2af5f743c0f9d28557
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-06 14:47:20 -0500

    creates a simple getallAlumnos mtheod to call the API REST

--- frontend/angular1/src/app/app.ts
+++ frontend/angular1/src/app/app.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Encabezado } from "./encabezado/encabezado";
 import { Formulario } from "./formulario/formulario";
 import { Listado } from "./listado/listado";
+import { MyService } from './my-service';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,18 @@ import { Listado } from "./listado/listado";
 export class App {
   protected readonly title = signal('angular1');
   rutaImagen = "public/img1.png";
+
+  private alumnoService = inject(MyService);
+
+  ngOnInit(): void {
+    this.loadAlumno();
+  }
+
+  private loadAlumno(): void {
+    this.alumnoService.getAllAlumnos().subscribe(alumnos => {
+      console.log(alumnos);
+    });
+  }
+
+  
 }
--- frontend/angular1/src/app/my-service.ts
+++ frontend/angular1/src/app/my-service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Alumno } from './alumno';
+import { catchError, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyService {
-  
+  private http = inject(HttpClient);
+  private readonly apiUrl = `http://localhost:8080/api/alumnos`;
+ 
+  getAllAlumnos(): Observable<Alumno[]> {
+    return this.http.get<Alumno[]>(this.apiUrl).pipe(
+      tap(alumnos => console.log('Fetched alumnos:', alumnos)),
+      catchError(() => {
+        console.log('Hubo error al obtener los alumnos');
+        return [];
+      })
+    );
+  }
+
+  createAlumno(){
+
+  }
+
+  updateAlumno(){
+
+  }
+
+  deleteAlumno(){
+
+  }
 }
