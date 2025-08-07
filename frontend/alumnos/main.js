b85d8e95bfc95b0d96ee98dd2c82b9d5cf0047cc
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-07 18:36:30 -0500

    restructure frontend folder to be only the angular app

--- frontend/alumnos/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import { AlumnoApp } from './resources/modules/app.js';
-
-// Inicialización de la aplicación
-document.addEventListener('DOMContentLoaded', async function() {
-    const app = new AlumnoApp();
-    await app.init();
-});
