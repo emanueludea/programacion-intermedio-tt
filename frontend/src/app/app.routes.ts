5a39d8201e20cd9f19dfc7cf66df374aa59819bc
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-07 23:56:28 -0500

    add route guards for estudiantes and cursos to enhance access control

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { Materias } from './domains/materias/pages/materias/materias';
 import { Matriculas } from './domains/matriculas/matriculas';
 import { Login } from './core/pages/login/login';
 import { Dashboard } from './core/pages/dashboard/dashboard';
+import { adminGuard } from './core/guards/admin-guard';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Dashboard, title: 'Inicio' },
     { path: 'login', component: Login, title: 'Iniciar sesión' },
-    { path: 'estudiantes', component: Alumnos, title: 'Estudiantes' },
-    { path: 'cursos', component: Materias, title: 'Cursos' },
+    { path: 'estudiantes', component: Alumnos, title: 'Estudiantes', canActivate: [adminGuard] },
+    { path: 'cursos', component: Materias, title: 'Cursos', canActivate: [authGuard] },
     { path: 'matriculas', component: Matriculas, title: 'Matriculas' },
+    { path: '**', redirectTo: '' },
 ];
