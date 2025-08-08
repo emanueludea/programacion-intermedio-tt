cb8b2501a900931d15992d34a534abfad3dad186
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-07 23:22:44 -0500

    update routing and navigation links for students, courses, and enrollments

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { Alumnos } from './domains/alumnos/pages/alumnos/alumnos';
 import { Materias } from './domains/materias/pages/materias/materias';
 import { Matriculas } from './domains/matriculas/matriculas';
+import { Login } from './core/pages/login/login';
+import { Dashboard } from './core/pages/dashboard/dashboard';
 
 export const routes: Routes = [
-    { path: '', component: Alumnos, title: 'Alumnos' },
-    { path: 'materias', component: Materias, title: 'Alumnos' },
-    { path: 'matriculas', component: Matriculas, title: 'Alumnos' },
+    { path: '', component: Dashboard, title: 'Inicio' },
+    { path: 'login', component: Login, title: 'Iniciar sesión' },
+    { path: 'estudiantes', component: Alumnos, title: 'Estudiantes' },
+    { path: 'cursos', component: Materias, title: 'Cursos' },
+    { path: 'matriculas', component: Matriculas, title: 'Matriculas' },
 ];
