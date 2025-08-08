25bd9824878d2ceee29a98d327bd351e26a8847e
Emanuel Montoya <emanuel.montoya@udea.edu.co>
2025-08-08 07:47:12 -0500

    feat: enhance student listing with search functionality and improved form layout using angular-bootstrap

--- frontend/src/app/domains/alumnos/pages/listado/listado.ts
+++ frontend/src/app/domains/alumnos/pages/listado/listado.ts
@@ -1,16 +1,42 @@
-import { Component, input, output } from '@angular/core';
+import { Component, input, output, PipeTransform } from '@angular/core';
 import { Alumno } from '../../models/alumno';
+import { map, Observable, startWith } from 'rxjs';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe, DecimalPipe } from '@angular/common';
+import { NgbHighlight } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-listado-alumnos',
-  imports: [],
+  imports: [DecimalPipe, AsyncPipe, ReactiveFormsModule, NgbHighlight],
   templateUrl: './listado.html',
-  styleUrl: './listado.css'
+  providers: [DecimalPipe],
 })
 export class Listado {
   alumnos = input<Alumno[]>([]);
   delete = output<Alumno>();
   update = output<Alumno>();
+  alumnos$: Observable<Alumno[]>;
+  filter = new FormControl('', { nonNullable: true });
+
+  constructor(pipe: DecimalPipe) {
+    this.alumnos$ = this.filter.valueChanges.pipe(
+      startWith(''),
+      map((text) => this.search(text, pipe)),
+    );
+  }
+  search(text: string, pipe: PipeTransform): Alumno[] {
+    return this.alumnos().filter((alumno) => {
+      const term = text.toLowerCase();
+      return (
+        alumno.primerNombre.toLowerCase().includes(term) ||
+        alumno.segundoNombre?.toLowerCase().includes(term) ||
+        alumno.primerApellido.toLowerCase().includes(term) ||
+        alumno.segundoApellido?.toLowerCase().includes(term) ||
+        pipe.transform(alumno.edad).includes(term) ||
+        pipe.transform(alumno.semestre).includes(term)
+      );
+    });
+  }
 
   editarAlumno(alumno: Alumno) {
     console.log("Editar alumno: " + alumno.cedula);
